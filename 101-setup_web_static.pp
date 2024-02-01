6fb02ddf30953dd8ea70fa509a23795555d11be7
japheth-alt163 <japhethbaraka4@gmail.com>
2024-02-01 08:25:56 -0800

    fire

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,63 +1,99 @@
-# File: 101-setup_web_static.pp
+# Configures a web server for deployment of web_static.
 
-# Ensure the necessary directories are created
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt',
+} ->
+
 file { '/data':
-  ensure => 'directory',
-}
+  ensure  => 'directory',
+} ->
 
 file { '/data/web_static':
   ensure => 'directory',
-}
+  require => File['/data'],
+} ->
 
-# Create the necessary subdirectories
-file { ['/data/web_static/releases', '/data/web_static/shared']:
+file { '/data/web_static/releases':
   ensure => 'directory',
-}
+  require => File['/data/web_static'],
+} ->
 
-# Create a symbolic link for 'current'
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+  require => File['/data/web_static/releases'],
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory',
+  require => File['/data/web_static'],
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n",
+  require => File['/data/web_static/releases/test'],
+} ->
+
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test',
+  require => File['/data/web_static'],
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/',
 }
 
-# Create an index.html file
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content => '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>',
-}
+file { '/var/www':
+  ensure => 'directory',
+} ->
 
-# Configure permissions
-file { '/data':
-  owner => 'ubuntu',
-  group => 'ubuntu',
-}
+file { '/var/www/html':
+  ensure => 'directory',
+} ->
 
-file { '/data/web_static':
-  owner => 'root',
-  group => 'root',
-}
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n",
+} ->
 
-file { '/data/web_static/releases':
-  owner => 'root',
-  group => 'root',
-}
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n",
+} ->
 
-file { '/data/web_static/shared':
-  owner => 'root',
-  group => 'root',
-}
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf,
+  notify => Exec['nginx_restart'],
+} ->
 
-file { '/data/web_static/current':
-  owner => 'root',
-  group => 'root',
-}
-
-file { '/data/web_static/releases/test':
-  owner => 'root',
-  group => 'root',
-}
-
-# Notify the user when the setup is complete
-notify { 'Web static setup complete':
-  message => 'Web static setup complete',
+service { 'nginx_restart':
+  command => 'service nginx restart',
+  require => File['/etc/nginx/sites-available/default'],
 }
